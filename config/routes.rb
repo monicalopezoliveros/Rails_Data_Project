f76fe4e7199e197df4493717d19ee3ffcee76b5e
mlopez <monicalopezoliveros@gmail.com>
2024-10-28 21:47:43 -0500

    Updated Cars and Views driver

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'cars/index'
+  resources :cars, only: [:index]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
