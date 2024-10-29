a8502371f18464b30c43704948af73a5904c416f
mlopez <monicalopezoliveros@gmail.com>
2024-10-28 23:57:36 -0500

    Main menu was added with home and about options

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+  get 'static/about'
   resources :cars, only: [:index, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # Rutas para Home y About
+  root 'cars#index' # O la acción que desees como página principal
+  get 'about', to: 'static#about' # Asumiendo que crearás un controlador llamado Static
+
 end
